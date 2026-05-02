9c461221b11a20bba171d933bc75a90c6fe84707
Link4Electronics <link4electronics@gmail.com>
2026-05-02 08:49:51 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,14 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
 export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./AppDir/bin/* \
+    /usr/lib/jvm/java*/bin \
+    /usr/lib/jvm/java*/conf \
+    /usr/lib/jvm/java*/legal \
+    /usr/lib/jvm/java*/lib
 
 # Additional changes can be done in between here
 
