884c9c2300a92936be437764212c112916515f0e
Link4Electronics <link4electronics@gmail.com>
2026-05-27 05:07:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,10 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/* \
-    /usr/lib/jvm/java*/bin \
-    /usr/lib/jvm/java*/conf \
-    /usr/lib/jvm/java*/legal \
-    /usr/lib/jvm/java*/lib
+    /usr/lib/jvm/java*
 
 # Additional changes can be done in between here
 
