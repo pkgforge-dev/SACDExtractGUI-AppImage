a32518cdd8d7d9fc239dfaccfabb3e3cd2f6b5ed
Link4Electronics <link4electronics@gmail.com>
2026-09-10 02:08:08 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,12 +16,9 @@ get-debloated-pkgs --add-common --prefer-nano
 
 echo "Getting app..."
 echo "---------------------------------------------------------------"
-wget https://github.com/setmind/SACDExtractGUI/releases/download/v0.1/SACDExtractGUI.zip
-git clone --depth 1 https://github.com/EuFlo/sacd-ripper
-
 mkdir -p ./AppDir/bin
-bsdtar -xvf SACDExtractGUI.zip -C ./AppDir/bin
-rm -f ./*.zip ./AppDir/bin/LICENSE ./AppDir/bin/README.md
+wget -P ./AppDir/bin https://github.com/rpmzine/SACDExtractGUI/raw/refs/heads/master/SACDExtractGUI.jar 
+git clone --depth 1 https://github.com/EuFlo/sacd-ripper
 cd sacd-ripper/tools/sacd_extract
 cmake -DCMAKE_BUILD_TYPE=Release .
 make -j$(nproc)
